47951fcb66ac5730286b920c700df0f10301890f
s3ptar <rene_graeber@graeber-elektronik.de>
2025-08-31 18:22:13 +0200

    Update download_roles.sh

--- bash/download_roles.sh
+++ bash/download_roles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/bin/curl -O https://github.com/s3ptar/ansible/tree/main/roles /tmp/semaphore/project_1/repository_1_template_5/update
+/usr/bin/curl -O https://github.com/s3ptar/ansible/tree/main/roles /tmp/semaphore/project_1/repository_1_template_5/update/roles
